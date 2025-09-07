66daafdd248b26d04d931db96c6d5511b1c73f58
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-07 20:03:33 +0000

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    strlen = len(str)
-    i = 0
-    while i < strlen:
+    for i in range(len(str)):
         char = ord(str[i])
         if 97 <= char <= 122:
             char -= 32
-        print("{}".format(chr(char)), end="" if i < strlen - 1 else "\n")
-        i += 1
+        print("{}".format(chr(char)), end="")
+    print()
