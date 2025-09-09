d3f6420613fcd30149eff0bcca930e2ec21d8ae8
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-09 18:38:34 +0000

    hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# prints the names in a compiled module
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    content = dir(hidden_4)
-    count = len(content)
-    for i in range(count):
-        if "__" in content[i]:
-            continue
-        print(content[i])
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
