92318056460271ff32af9656b5ad19f5958ba020
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-09 15:25:56 +0000

    infinite_add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__name__":
+    count = len(sys.argv)
+    count -= 1
+    result = 0
+    if count == 0:
+        print("{}".format(count))
+    else:
+        for i in range(1, len(sys.argv)):
+            result += int(sys.argv[i])
+        print("{}".format(result))
