34535e433a250de59f807f10c248cb554b39853c
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-14 06:02:35 +0000

    max_integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    large = my_list[0]
+    for num in my_list:
+        if num > large:
+            large = num
+    return large
