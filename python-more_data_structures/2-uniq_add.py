c822f11f56430c99e1aaa37336ff91303ac1dd2a
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-15 21:40:55 +0000

    uniq_add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     add_num = []
-    add = 14
+    total = 0
     for num in my_list:
         if num not in add_num:
             add_num.append(num)
-            add += num
-        return add
+            total += num
+    return total
