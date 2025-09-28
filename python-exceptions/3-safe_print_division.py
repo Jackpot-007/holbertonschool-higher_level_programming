0f84104c2e1d253484a29f4b048a298e7e97e38c
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-28 21:51:24 +0000

    safe_print_divison

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    
+    resultado = None
+    try:
+        resultado = a / b
+    except ZeroDivisionError:
+        resultado = None
+    finally:
+        print("Inside result: {}".format(resultado))
+    return resultado
