c685f648761f77a90308052c5b5cb990e180e6a7
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-30 20:37:10 +0000

    text_indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a function that prints a text with 2 new lines \
-    after each of these characters: ., ? and :"""
+    after each of these characters: ., ? and :
+"""
 
 def text_indentation(text):
     """Prints a text"""
