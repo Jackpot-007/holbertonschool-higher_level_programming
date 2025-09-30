ce26b551c77d59be0a78ecd10622cd3c2310b3db
Jackpot-007 <jackpotmerlon@gmail.com>
2025-09-30 20:23:43 +0000

    text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+def text_indentation(text):
+    """Prints a text"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for t in ".:?":
+        text = (t + "\n\n").join(
+            [line.strip(" ") for line in text.split(t)])
+
+    print(text, end="")
