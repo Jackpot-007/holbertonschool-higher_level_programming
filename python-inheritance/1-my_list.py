273d4cf102e685b14b0a8bdef5123ae09f938a0a
Jackpot-007 <jackpotmerlon@gmail.com>
2025-10-01 17:25:45 +0000

    my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Define a class MyList that inherits from list:"""
+
+class MyList(list):
+    """Class that inherits from list"""
+    def print_sorted(self):
+        list_ = self[:]
+        list_.sort()
+        print(list_)
