616926b11e89a3fb85c101365f2d8e8ee6365287
Jackpot-007 <jackpotmerlon@gmail.com>
2025-10-02 05:17:42 +0000

    base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry\
+(based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry
+    """
+    def area(self):
+        """Area function
+        """
+        raise Exception("area() is not implemented")
