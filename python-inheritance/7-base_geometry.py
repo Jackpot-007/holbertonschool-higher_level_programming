350216dcf82206ee27c89ef58a77e254839c4ed5
Jackpot-007 <jackpotmerlon@gmail.com>
2025-10-02 05:35:57 +0000

    base_geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry\
+(based on 6-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry
+    """
+    def area(self):
+        """Area function
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
