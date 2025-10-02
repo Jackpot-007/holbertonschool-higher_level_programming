448775ee6ebab4ada7e2e0ae0b94c6f9de9e50b3
Jackpot-007 <jackpotmerlon@gmail.com>
2025-10-02 14:53:09 +0000

    rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Creates new instances of rectangle"""
 
-        self.__width = width
-        self.__height = height
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
