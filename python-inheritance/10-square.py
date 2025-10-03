ece8f663811514d641f62c0ddc31c92088bddae0
Jackpot-007 <jackpotmerlon@gmail.com>
2025-10-03 16:04:58 +0000

    square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
-"""Defines a class Square based on 9-rectangle.py.
-
-Attributes:
-    width (int): width of the rectangle.
-    height (int): height of the rectangle.
-"""
+"""Defines a class Square based on 9-rectangle.py."""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square.
-
-    Args:
-        Rectangle (Rectangle): rectangle
-    """
+    """Defines a class Square."""
 
     def __init__(self, size):
-        """Creates new instances of class Square.
-
-        Args:
-            size (int): size of 1 side of square.
-        """
+        """Creates new instances of class Square."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Calculates the area of a square.
-
-        Returns:
-            int: the area of the square.
-        """
+        """Calculates the area of a square."""
         return self.__size ** 2
